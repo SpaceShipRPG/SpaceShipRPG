4f1733e712630b458546ac8eed7274592fcdc923
a-langner <andreas_langner@yahoo.de>
2021-10-25 18:53:43 +0200

    kleine Korrekturen

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//Note, ensure each map has a border of dots (this makes game logic simpler)
 const staticMap = [  
     "--------------------S", //0 (Starting map!)
     "---------------------", //1
@@ -25,11 +24,11 @@ const staticMap = [
 ];
 
 
-// - = nothing
-// S = ship graveyard
+// - = Void / Emptiness / Nothing
+// S = Ship Graveyard
 // P = Planet
 // T = Space Station
-// A = Asteroid Field
+// A = Asteroid Belt
 // X = Pirates
 // N = Nebula
 // H = Home
@@ -37,57 +36,73 @@ const staticMap = [
 // B = Black Hole
 
 
+function void() {
+    console.log('Here is only void space.');
+    // Rückgabewert --> was passiert? 
+};
+
 function sun() {
   console.log('Here is a sun.');
   // Rückgabewert --> was passiert? 
-}
+};
 
 function planet() {
   console.log('Here is a planet.');
   // Rückgabewert --> was passiert? 
-}
+};
+
+function home() {
+    console.log('Welcome to your home planet.');
+    // Rückgabewert --> was passiert? 
+};
+
 function astroidBelt() {
   console.log('Here is a astroid belt. Be careful!');
   // Rückgabewert --> was passiert? 
-}
-function fog() {
-  console.log('Here is a fog. Be careful!');
+};
+
+function nebula() {
+  console.log('Here is a nebula. Be careful!');
   // Rückgabewert --> was passiert? 
-}
+};
+
 function spaceStation() {
   console.log('Here is a space station.');
   // Rückgabewert --> was passiert? 
-}
-function cemetery() {
-  console.log('Here is a cemetery for space ships.');
+};
+
+function graveyard() {
+  console.log('Here is a ship graveyard for space ships.');
   // Rückgabewert --> was passiert? 
-}
+};
+
 function pirate() {
   console.log('Here are Pirates.');
   // Rückgabewert --> was passiert? 
-}
+};
+
 function blackHole() {
   console.log('Here is a black hole');
   // Rückgabewert --> was passiert? 
-}
+};
+
 function xpUp(params) {
-  
-}
-function xpDown(params) {
-  
-}
+};
+
+function xpDown(params) { 
+};
+
 function fuelUp(params) {
-  
-}
-function resource(params) {
-  
-}
+};
+
+function resource(params) { 
+};
+
 function statusFuel(params) {
-  
-}
+};
+
 function pickUp(params) {
-  
-}
+};
+
 function delivery(params) {
-  
-}
+};
