77b31bf0c0833882b72158c390d35a05db0b9070
Moh-Khosravi <mohammad.khosravi@gmx.de>
2021-10-26 15:46:00 +0200

    funktionsfähige bewegung

--- direction.js
+++ direction.js
@@ -21,23 +21,20 @@ function movement (order) {
     }
 } */
 
-let shipPosition = {
-    x : 11,
-    y : 11
-}
+let shipPosition = [11, 11];
 function movement (order) {
     const newPosition = [];
     if(order === 8) {
-        newPosition.push(shipPosition.x, shipPosition.y+1)
+        newPosition.push(shipPosition[0], shipPosition[1]+1)
 
     } else if(order === 4){
-        newPosition.push(shipPosition.x-1, shipPosition.y)
+        newPosition.push(shipPosition[0]-1, shipPosition[1])
 
     } else if(order === 2){
-        newPosition.push(shipPosition.x, shipPosition.y-1)
+        newPosition.push(shipPosition[0], shipPosition[1]-1)
 
     } else if(order === 6) {
-        newPosition.push(shipPosition.x+1, shipPosition.y)
+        newPosition.push(shipPosition[0]+1, shipPosition[1])
     }
     return newPosition;
 }
@@ -46,12 +43,18 @@ const meineBewegung = movement(Number(prompt('')));
 const meineBewegungX = meineBewegung[0];
 const meineBewegungY = meineBewegung[1];
 
-const laenge = staticMap.length;
+let clash = false;
 
 for (let elem of staticMap) {
-  if (elem.x === meineBewegungX && elem.y === meineBewegungY) {
-    console.log(`${elem.name} is in the way. The coordinates of your current position are x: ${meineBewegungX}, y: ${meineBewegungY}.`);
-  }
-  console.log(`You are in a empty space and the coordinates of your position are x: ${meineBewegungX}, y: ${meineBewegungY}.`);
-  break;
+    if (elem.x === meineBewegungX && elem.y === meineBewegungY) {
+      console.log(`${elem.name} is in the way. The coordinates of your current position are x: ${shipPosition[0]}, y: ${shipPosition[1]}.`);
+      clash = true;
+    } 
 }
+if (!clash) {
+    shipPosition[0] = meineBewegungX;
+    shipPosition[1] = meineBewegungY;
+    console.log(`You are in an empty space and the coordinates of your position are x: ${shipPosition[0]}, y: ${shipPosition[1]}.`);
+}
+    
+    
