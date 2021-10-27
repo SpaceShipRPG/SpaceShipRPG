ff4bad252510d48e3a9548148b142285ba38006f
Moh-Khosravi <mohammad.khosravi@gmx.de>
2021-10-27 10:18:54 +0200

    Umstruckturierung_27.10.2021

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const Ship = require('./alteDateien/Ship.js')
+/* const Ship = require('./alteDateien/Ship')
 
 const testSchiff = new Ship ('Test-Schiff', 200, {shields: 50}, {water: 25, oxygen: 20, energy: 30, food: 25})
 
-console.log(neue)
+console.log(neue) */
