44bd7c5c2cb19d08698f825e60a0b5dc9d02d114
Moh-Khosravi <mohammad.khosravi@gmx.de>
2021-10-29 16:18:11 +0200

    letzte version des Tages 291021

--- files/Comerce.js
+++ files/Comerce.js
@@ -1,61 +1,60 @@
-const playerShip = require('./PlayerShip.js');
+const PlayerShip = require('./PlayerShip.js');
 function sell(what, quantity) {
 
-    PlayerShip.getMoney = quantity
+    PlayerShip.playerShip.getMoney = quantity
 
     switch (what) {
         case 'mineral':
             if (quantity > this.resources.mineral) {
                 quantity === this.resources.mineral
             }
-            PlayerShip.sellMineral = quantity
+            PlayerShip.playerShip.sellMineral = quantity
             break;
         case 'metal':
             if (quantity > this.resources.metal) {
                 quantity === this.resources.metal
             }
-            PlayerShip.sellMetal = quantity
+            PlayerShip.playerShip.sellMetal = quantity
             break;
         case 'gas':
             if (quantity > this.resources.gas) {
                 quantity === this.resources.gas
             }
-            PlayerShip.sellGas = quantity
+            PlayerShip.playerShip.sellGas = quantity
             break;
         case 'tech':
             if (quantity > this.resources.tech) {
                 quantity === this.resources.tech
             }
-            PlayerShip.sellTech = quantity
+            PlayerShip.playerShip.sellTech = quantity
             break;
     }
 }
 
 function buy (what, quantity) {
     
-    PlayerShip.investMoney = quantity
-    if (quantity > this.resources.money) {
-        quantity === money
+    PlayerShip.playerShip.investMoney = quantity
+    if (quantity > PlayerShip.playerShip.resources.credits) {
+        quantity === credits
     }
 
     switch (what) {
         case 'water':
-            PlayerShip.buyWater = quantity
+            PlayerShip.playerShip.buyWater = quantity;
             break;
         case 'food':
-            PlayerShip.buyFood = quantity
+            PlayerShip.playerShip.buyFood = quantity
             break;
         case 'drugs':
-            PlayerShip.buyDrugs = quantity
+            PlayerShip.playerShip.buyDrugs = quantity
             break;
         case 'shipParts':
-            PlayerShip.buyShipParts = quantity
+            PlayerShip.playerShip.buyShipParts = quantity
             break;
         case 'energy':
-            PlayerShip.buyEnergy = quantity
+            PlayerShip.playerShip.buyEnergy = quantity
             break;
     }
 }
-
 exports.sell = sell;
 exports.buy = buy;
