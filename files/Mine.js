7b9848fde2ac0d6d56fb4e95fc50b2cc02248724
Moh-Khosravi <mohammad.khosravi@gmx.de>
2021-11-01 00:30:30 +0100

    correction

--- files/Mine.js
+++ files/Mine.js
@@ -4,10 +4,10 @@ const randomNr = require('./randomNr.js');
 function mine (what) {
     const storageCapacity = PlayerShip.playerShip.resources[what][1] - PlayerShip.playerShip.resources[what][0]; 
     let amount = Math.floor(PlayerShip.playerShip.crew[0] * randomNr(0.8, 1.2,1) / 10);
-    console.log(randomNr(0.8,1.2,1));
+
     //let amount = Math.floor(this.crew * randomNr(0.8, 1.2) / 10);
 
-     // wenn amount größer ist als Lagerkapazität, dann Lagerkapazität, sonst amount.
+    // wenn amount größer ist als Lagerkapazität, dann Lagerkapazität, sonst amount.
     amount = (amount > storageCapacity) ? storageCapacity : amount;
     PlayerShip.playerShip.buy(what, amount);
 };
