49d355db6cc51bc1669ca487f6f41b3213342028
Moh-Khosravi <mohammad.khosravi@gmx.de>
2021-11-01 13:11:11 +0100

    v3-011121

--- files/Comerce.js
+++ files/Comerce.js
@@ -1,36 +1,10 @@
 const PlayerShip = require('./PlayerShip.js');
 function sell(what, quantity) {
-
-/*    switch (what) {
-        case 'mineral':
-            if (quantity > PlayerShip.playerShip.resources.minerals[0]) {
-                quantity = PlayerShip.playerShip.resources.minerals[0]
-            }
-            PlayerShip.playerShip.sell("minerals", quantity)
-            break;
-        case 'metal':
-            if (quantity > PlayerShip.playerShip.resources.metal[0]) {
-                quantity = PlayerShip.playerShip.resources.metal[0]
-            }
-            PlayerShip.playerShip.sell("metal", quantity)
-            break;
-        case 'gas':
-            if (quantity > PlayerShip.playerShip.resources.gas[0]) {
-                quantity = PlayerShip.playerShip.resources.gas[0]
-            }
-            PlayerShip.playerShip.sell("gas", quantity)
-            break;
-        case 'specialTech':
-            if (quantity > PlayerShip.playerShip.resources.specialTech[0]) {
-                quantity = PlayerShip.playerShip.resources.specialTech[0]
-            }
-            PlayerShip.playerShip.sell("specialTech", quantity)
-            break;
-    }
-*/
+    // hier wird überprüft ob die Menge, die wir verkaufen wollen größer ist als die Menge, die vorhanden ist. Falls ja, dann kann/wird nur die gesamte vorhandene Menge verkauft. 
     quantity = quantity > PlayerShip.playerShip.resources[what][0] ? PlayerShip.playerShip.resources[what][0] : quantity;
-
+    // hier wird verkauft
     PlayerShip.playerShip.sell(what, quantity);
+    // hier erhalten wir Geld
     PlayerShip.playerShip.buy("credits", quantity);
 }
 
@@ -39,25 +13,6 @@ function buy (what, quantity) {
     quantity = quantity > PlayerShip.playerShip.resources.credits[0] ? PlayerShip.playerShip.resources.credits[0] : quantity;
     // hier wird überprüft wie viel 'what' gekauft werden kann in abhängigkeit der Lagerungskapazität.  
     quantity = (quantity > PlayerShip.playerShip.resources[what][1] - PlayerShip.playerShip.resources[what][0]) ? (PlayerShip.playerShip.resources[what][1] - PlayerShip.playerShip.resources[what][0]) : quantity;
-/*    switch (what) {
-        case 'water':
-            PlayerShip.playerShip.buy("water", quantity);
-            break;
-        case 'food':
-            PlayerShip.playerShip.buy("food", quantity);
-            break;
-        case 'drugs':
-            PlayerShip.playerShip.buy("drugs", quantity);
-            break;
-        case 'shipParts':
-            // diese Variable existiert nicht.
-            PlayerShip.playerShip.buy("shipParts", quantity);
-            break;
-        case 'energy':
-            PlayerShip.playerShip.buy("energy", quantity);
-            break;
-    }
-*/
     // hier wird gekauft
     PlayerShip.playerShip.buy(what, quantity);
     // hier wird bezahlt
