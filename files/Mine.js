dbfa7ebf94a87b0632594dba34ee1e11d9153dcb
Moh-Khosravi <mohammad.khosravi@gmx.de>
2021-11-01 09:27:17 +0100

    änderung v1 01.11.21

--- files/Mine.js
+++ files/Mine.js
@@ -5,8 +5,6 @@ function mine (what) {
     const storageCapacity = PlayerShip.playerShip.resources[what][1] - PlayerShip.playerShip.resources[what][0]; 
     let amount = Math.floor(PlayerShip.playerShip.crew[0] * randomNr(0.8, 1.2,1) / 10);
 
-    //let amount = Math.floor(this.crew * randomNr(0.8, 1.2) / 10);
-
     // wenn amount größer ist als Lagerkapazität, dann Lagerkapazität, sonst amount.
     amount = (amount > storageCapacity) ? storageCapacity : amount;
     PlayerShip.playerShip.buy(what, amount);
